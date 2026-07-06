9f546e4473fecd4d0e905705b55c302629d2f0cf
[장유지][기술개발본부][사원] <yuji918@datau.co.kr>
2026-07-06 10:56:32 +0900

    v2.0 VerificationCodeInput css 수정

--- src/shared/components/du-admin-ui/VerificationCodeInput/index.tsx
+++ src/shared/components/du-admin-ui/VerificationCodeInput/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import VerificationInput from "react-verification-input";
+import VerificationInput from 'react-verification-input'
 import styled from 'styled-components'
 
 import { FONT_SIZE, FONT_WEIGHT, media } from '@/styles/themes/constants'
@@ -31,67 +31,75 @@ export const VerificationCodeInput: React.FC<VerificationCodeInputProps> = (prop
   }
 
   return (
-    <S.CodeInput
-      value={values}
-      classNames={{
-        container: 'container',
-        character: 'character',
-        characterInactive: 'character--inactive',
-        characterSelected: 'character--selected',
-      }}
-      placeholder={''}
-      {...props}
-      onChange={handelChange}
-    />
+    <S.CodeInput>
+      <VerificationInput value={values} placeholder={''} {...props} onChange={handelChange} />
+    </S.CodeInput>
   )
 }
 
 const S = {
-  CodeInput: styled(VerificationInput)`
-    display: flex;
-    gap: 0.625rem;
+  CodeInput: styled.div`
+    .vi__container {
+      display: flex;
+      gap: 0.625rem;
+      width: 15.625rem;
+      height: 5rem;
+    }
 
-    .character {
+    .vi__character {
       line-height: 5rem;
       font-size: ${FONT_SIZE.xxl};
       font-weight: ${FONT_WEIGHT.bold};
       color: var(--text-main-color);
       border: 3px solid var(--lightgrey);
       border-radius: 0.625rem;
+      background-color: transparent;
       width: 3.4375rem;
       height: 5rem;
     }
 
-    .character--inactive {
+    .vi__character--inactive {
       border: 3px solid var(--lightgrey);
     }
 
-    .character--selected {
+    .vi__character--selected {
       border: 3px solid var(--primary-color);
     }
 
     @media only screen and (${media.xs}) {
-      gap: 0.4375rem;
-      .character {
+      .vi__container {
+        gap: 0.4375rem;
+        width: 11.5625rem;
+        height: 3.75rem;
+      }
+      .vi__character {
         width: 2.5625rem;
         height: 3.75rem;
       }
     }
 
     @media only screen and (${media.md}) {
-      gap: 0.625rem;
-      .character {
+      .vi__container {
+        gap: 0.625rem;
+        width: 15.625rem;
+        height: 5rem;
+      }
+      .vi__character {
         width: 3.4375rem;
         height: 5rem;
       }
     }
 
     @media only screen and (${media.lg}) {
-      gap: 0.625rem;
-      .character {
+      .vi__container {
+        gap: 0.625rem;
+        width: 15.625rem;
+        height: 5rem;
+      }
+      .vi__character {
         width: 3.4375rem;
         height: 5rem;
       }
     }
-  `
+  `,
 }
